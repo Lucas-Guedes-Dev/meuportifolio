1e19d2e907fb1db9fec4d4a33b5801d0e60e4fc5
Lucas Guedes <lucas_guidolinguedes@outlook.com>
2023-11-28 17:37:30 -0300

    suibindo

--- portifolio/settings.py
+++ portifolio/settings.py
@@ -127,7 +127,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'portapp', 'static'),
 ]
-print(STATICFILES_DIRS)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
