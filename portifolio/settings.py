d6ee06f0e896e7390a3f9f5ac3acbe5d0772de36
Lucas Guedes <lucas_guidolinguedes@outlook.com>
2023-11-28 12:37:52 -0300

    static

--- portifolio/settings.py
+++ portifolio/settings.py
@@ -122,10 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/css/style.acss')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'portapp', 'static'),
 ]
+print(STATICFILES_DIRS)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
