bc796362ea6c43c21fa63cbac04d49f807fc14fa
Lucas Guedes <lucas_guidolinguedes@outlook.com>
2023-11-28 11:22:54 -0300

    Changes to be committed: 	modified:   portapp/static/css/style.css 	modified:   portapp/templates/components/home.html 	modified:   portifolio/settings.py

--- portifolio/settings.py
+++ portifolio/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+rh@1*#bk+k1a_vl4$&+s0p=80+km@o*u*8$-r-5riw$x9ph$k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['192.168.0.211']
+# ALLOWED_HOSTS = ['192.168.0.211']
 
 
 # Application definition
