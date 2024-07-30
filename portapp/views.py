4f2c2aee22f97d2607b0a68f588ed118d0d82f41
guidolinzinho <lucas_guidolinguedes@outlook.com>
2024-07-29 22:29:07 -0300

    arrumando tudo

--- portapp/views.py
+++ portapp/views.py
@@ -1,26 +1,26 @@
-from typing import Any
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
+from django.contrib import messages
+
 from .forms import LeadForm
+
 
 class Index(View):
     def as_view(self, request):
         return render(request, 'components/home.html')
-    
+
+
 class Contato(View):
     def as_view(self, request):
-
         if request.method == 'POST':
             form = LeadForm(request.POST)
-
             if form.is_valid():
-                
-                obj = form.save(commit=True)
-                
-                obj.save()
+                form.save()
+                messages.success(request, 'Formulário salvo com sucesso!')
+                # Redirecionar para a mesma página para evitar o reenvio do formulário
+                return redirect('contato')
         else:
             form = LeadForm()
 
-        context = {'form' : form}
-
+        context = {'form': form}
         return render(request, 'components/contato.html', context)
